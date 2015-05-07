0c2ddcecc70a25c4bef43e202adc0915bd5e33d9
Vladimir Petkov <vlado@itgbg.com>
2015-05-08 01:05:16 +0300

    improving the interface, fixing cancel_reservation and adding show_movie_pprojections

--- cli.py
+++ cli.py
@@ -2,17 +2,28 @@ import logic
 
 while True:
     command = input("command>")
-    if command == "show_movies":
+    command = command.split(sep=" ")
+
+    if command[0] == "show_movies":
         logic.show_movies()
-    elif command == "show_movie_projections":
-        logic.show_movie_projections()
-    elif command == "make_reservation":
+
+    elif command[0] == "show_movie_projections":
+        if len(command) == 2:
+            logic.show_movie_projections(int(command[1]))
+        elif len(command) == 3:
+            logic.show_movie_projections(int(command[1]), date=int(command[2]))
+
+    elif command[0] == "make_reservation":
         logic.make_reservation()
-    elif command == "cancel_reservation":
-        logic.cancel_reservation()
-    elif command == "help":
+
+    elif command[0] == "cancel_reservation":
+        logic.cancel_reservation(command[1])
+
+    elif command[0] == "help":
         logic.helpp()
-    elif command == "abracadabra":
+
+    elif command[0] == "abracadabra":
         logic.abracadabra()
-    elif command == "exit":
+
+    elif command[0] == "exit":
         break
--- logic.py
+++ logic.py
@@ -4,16 +4,14 @@ import time
 
 
 def helpp():
-    print("""
-You can use the following magic commands for our magic cinema:
+    print("""You can use the following magic commands for our magic cinema:
 show_movies
-show_movie_projection
+show_movie_projections <movie_id> [<date>]
 make_reservation
-cancel_reservation
+cancel_reservation <name>
 help
 exit
-abracadabra
-""")
+abracadabra""")
 
 
 def abracadabra():
@@ -25,17 +23,30 @@ def abracadabra():
 
 def show_movies():
     print("Current movies:")
-    conn = sqlite3.connect("Movies.db")
+    conn = sqlite3.connect("cinema.db")
     cursor = conn.cursor()
     result = cursor.execute("SELECT name FROM Movies ORDER BY rating")
-    for row in result:
+    for row in result.fetchall():
         print(row)
 
-def cancel_reservation(name):
-    conn = sqlite3.connect("Movies.db")
+
+def cancel_reservation(name):  # we have to handle the case where the name is not in reservation table
+    conn = sqlite3.connect("cinema.db")
     cursor = conn.cursor()
-    result = cursor.execute("DELETE FROM Reservations WHERE username = name")
+    cursor.execute("DELETE FROM Reservations WHERE username = \"{}\"".format(name))
+    conn.commit()
 
 
-def show_movie_projections():
-    pass
+def show_movie_projections(movie_id, date=None):  # current function doesn't print the remaining free seats
+    conn = sqlite3.connect("cinema.db")
+    cursor = conn.cursor()
+    if date is None:
+        result = cursor.execute("SELECT proj_id, date, time, type FROM projections WHERE movie_id = {} ORDER BY date".format(movie_id))
+    else:
+        result = cursor.execute("SELECT proj_id, time, type FROM projections WHERE movie_id = {} AND date = {} ORDER BY date".format(movie_id, date))
+    for row in result.fetchall():
+        print(row)
+
+# Happy coding :)
+# ^ ^
+# ---
